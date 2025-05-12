21d6e3847cdca11cbec3702f5322887186f36f64
Jose Carlos <cuentatrabajo2605@gmail.com>
2025-05-12 12:37:26 -0500

    Agregado redireccionamiento con confirmación al formulario de descuento

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 setTimeout(() => {
-  // Reemplaza con tu enlace real al formulario de Google
-  window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSeHtaKN7-218pPcd7ZuuqJTFGOEZZLDFhHDdqhDMMMFLHBGJg/viewform?embedded=true";
+  const confirmar = confirm("¿Deseas registrarte para recibir un descuento en tu próxima visita?");
+  if (confirmar) {
+    window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSeHtaKN7-218pPcd7ZuuqJTFGOEZZLDFhHDdqhDMMMFLHBGJg/viewform";
+  }
 }, 3000);
-
-
