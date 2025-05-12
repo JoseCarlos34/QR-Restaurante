19842569431ef68621ef7eca80087b920f2968f7
Jose Carlos <cuentatrabajo2605@gmail.com>
2025-05-12 14:42:30 -0500

    Actualizacion 2

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 setTimeout(() => {
-  const confirmar = confirm("¿Deseas registrarte para recibir un descuento en tu próxima visita?");
-  if (confirmar) {
-    window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSeHtaKN7-218pPcd7ZuuqJTFGOEZZLDFhHDdqhDMMMFLHBGJg/viewform";
-  }
+  document.getElementById("modal").style.display = "flex";
 }, 3000);
+
+document.getElementById("aceptarBtn").addEventListener("click", () => {
+  window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSeHtaKN7-218pPcd7ZuuqJTFGOEZZLDFhHDdqhDMMMFLHBGJg/viewform";
+});
+
+document.getElementById("cancelarBtn").addEventListener("click", () => {
+  document.getElementById("modal").style.display = "none";
+});
