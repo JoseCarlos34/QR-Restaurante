39377655d8f9f57f8294415e1875fac66c504dd2
Jose Carlos <cuentatrabajo2605@gmail.com>
2025-05-12 12:24:07 -0500

    Actualización de carta con emojis y diseño responsive

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ setTimeout(() => {
   // Reemplaza con tu enlace real al formulario de Google
   window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSeHtaKN7-218pPcd7ZuuqJTFGOEZZLDFhHDdqhDMMMFLHBGJg/viewform?embedded=true";
 }, 3000);
+
+
