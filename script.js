b7fbc6f1d5fa783a775754cba6377b539db60cc6
Jose Carlos <cuentatrabajo2605@gmail.com>
2025-05-12 11:34:54 -0500

    Primer commit - Proyecto QR Restaurante

---
+++ script.js
@@ -0,0 +1,4 @@
+setTimeout(() => {
+  // Reemplaza con tu enlace real al formulario de Google
+  window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSeHtaKN7-218pPcd7ZuuqJTFGOEZZLDFhHDdqhDMMMFLHBGJg/viewform?embedded=true";
+}, 3000);
